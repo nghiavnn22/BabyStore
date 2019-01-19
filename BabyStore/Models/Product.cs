7a54606016912ead2cecb17cadff14e7d926e892
nghiavnn22 <nguyenhuunghia1994@gmail.com>
2019-01-20 00:08:39 +0700

    Add project files.

---
+++ BabyStore/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BabyStore.Models
+{
+    public partial class Product
+    {
+        public int ID { get; set; }
+        [Display(Name="Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int? CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
